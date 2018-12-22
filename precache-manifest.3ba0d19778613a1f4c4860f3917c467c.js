f61a1c0babdc9b474ac5b2928a69930f2828d104
unknown <mausam.singh@liferay.com>
2018-12-22 23:39:20 +0530

    Updates

---
+++ precache-manifest.3ba0d19778613a1f4c4860f3917c467c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbf8307d039a69e8f36e",
+    "url": "/mausamdotsingh/contactmanager/static/js/runtime~main.dbf8307d.js"
+  },
+  {
+    "revision": "8e6ebc7c9614877ee49f",
+    "url": "/mausamdotsingh/contactmanager/static/js/main.8e6ebc7c.chunk.js"
+  },
+  {
+    "revision": "d31a1afc797f8d67d8ad",
+    "url": "/mausamdotsingh/contactmanager/static/js/1.d31a1afc.chunk.js"
+  },
+  {
+    "revision": "8e6ebc7c9614877ee49f",
+    "url": "/mausamdotsingh/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "d31a1afc797f8d67d8ad",
+    "url": "/mausamdotsingh/contactmanager/static/css/1.fc1546eb.chunk.css"
+  },
+  {
+    "revision": "4801b83c9fe75289a6f05670a0fbdf6c",
+    "url": "/mausamdotsingh/contactmanager/index.html"
+  }
+];
